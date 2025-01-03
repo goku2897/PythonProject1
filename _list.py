0e62710e0b2df730c6605abd55307b59bed9e996
rajeshpadhy <rpadhy98@gmail.com>
2025-01-03 18:15:00 +0530

    Day 3 learning

---
+++ _list.py
@@ -0,0 +1,12 @@
+x = 10
+y = "training"
+
+z = [10,20,30,40]
+a = ["str","Men","rcv","academy"]
+b = [10,"20",40,50.0]
+
+# In List duplicate value are allowed
+print(a[2]) # rcv
+print(type(b)) # <class 'list'>
+
+print(b[0:3:2])
--- string_function.py
+++ string_function.py
@@ -1,49 +1,55 @@
 x = "rcv academy rcv academy rcv academy"
-y = 9938602399
-print(len(x))
-print(str(y))
-z= str(y)
-print(z.find("38"))
-
-#Upper
-a = print(x.upper())
-a = x.upper()
-
-#count
-print(x.count("rcv",10,30))
-
-
-#isupper
-print(a.isupper())
-
-#islower
-print(a.islower())
-
-
-#split
-print(x.split()) #['rcv', 'academy', 'rcv', 'academy', 'rcv', 'academy'] based on space is we doesnt provide ant value in the function
-
-#strip
-u = "     ;;;;;;rcv academy rcv academy rcv academy   "
-print(u.strip(';7 \''))
+# y = 9938602399
+# print(len(x))
+# print(str(y))
+# z= str(y)
+# print(z.find("38"))
+#
+# #Upper
+# a = print(x.upper())
+# a = x.upper()
+#
+# z = "RTYUIO"
+# print(z.lower())
+#
+# #count
+# print(x.count('rcv'))
+# print(x.count("rcv",10,30))
+#
+#
+# #isupper
+# print(a.isupper())
+#
+# #islower
+# print(a.islower())
+#
+#
+# #split
+# print(x.split()) #['rcv', 'academy', 'rcv', 'academy', 'rcv', 'academy'] based on space is we doesnt provide ant value in the function
+# print(x.split('v'))
+#
+# #strip
+# u = "     ;;;;;;rcv academy rcv academy rcv academy   "
+# print(u.strip())
+# print(u.strip(';7 \''))
 
 #lstrip
-u = "     ;;;;;;rcv academy rcv academy rcv academy   "
+u = "     ;;;;;;rcv academy rcv academy rcv academy''''   "
 print(u.lstrip(';7 \''))
 
 
 #rstrip
-u = "     ;;;;;;rcv academy rcv academy rcv academy   "
+u = "     ;;;;;;rcv academy rcv academy rcv academy'''''   "
 print(u.rstrip(';7 \''))
 
 #replace
 print(u.replace("rcv","stm",2))
 
 #find
-print(u.find("rcv"))
+print(x.find("rcv",10,30))
 
 #index
-print(u.index("rcv"))
+print(x.index("rcv"))
 
 
 
