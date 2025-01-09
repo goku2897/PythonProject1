5d12cbefa29b9261f95d19ba0562e2352c87e8ee
rajeshpadhy <rpadhy98@gmail.com>
2025-01-09 21:57:27 +0530

    last 4 days learning

---
+++ for_loop_1.py
@@ -0,0 +1,33 @@
+# city = "Delhi"
+# for c in city:
+#     print(c)
+
+# city = ["Delhi","Melbourne","New York","Lucknow"]
+# for c in city:
+#     if c == "Delhi":
+#      print(c)
+
+# city = ("Delhi","Melbourne","New York","Lucknow")
+# for c in city:
+#     print(c)
+
+#list in list
+cities = [["India","Delhi"],["Australia","Melbourne"],["Canada","Vancouver"],["USA","New York"]]
+
+
+for county in cities:
+    print(county)
+
+#     print("Country is %s and city is %s" %(county,city))
+#
+# #set
+# city = {"Delhi","Melbourne","New York","Lucknow"}
+# for c in city:
+#     print(c)
+
+#dict
+# my_dictionary = dict(cities)
+# for county,city in my_dictionary.items():
+#     print(county,city)
+#     for s in county:
+#         print(s)
---
+++ for_loop_2.py
@@ -0,0 +1,17 @@
+# #print 1 to 10
+# #range(start position, end position)
+# for i in range(1,10):
+#     print(i)
+
+
+
+#print 1 to 20
+
+# for i in range(1,21,2):
+#     print(i)
+
+
+#print 10 to 1
+
+for i in range(10,0,-1):
+    print(i)
